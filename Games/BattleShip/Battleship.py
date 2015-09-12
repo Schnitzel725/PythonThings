15ec675c6fd117788c6c36348cfb9dd4d447f32a
XRuizhe <ruizhe1997@gmail.com>
2015-09-12 18:22:10 -0500

    Create Battleship.py

---
+++ Games/BattleShip/Battleship.py
@@ -0,0 +1,48 @@
+#Code for CommandLine Battleship written in Python
+#Inspired by Codeacademy Python tutorials
+
+from random import randint
+
+board = []
+
+for x in range(5):
+    board.append(["O"] * 5)
+
+def print_board(board):
+    for row in board:
+        print " ".join(row)
+
+print "Let's play Battleship!"
+print_board(board)
+
+def random_row(board):
+    return randint(0, len(board) - 1)
+
+def random_col(board):
+    return randint(0, len(board[0]) - 1)
+
+ship_row = random_row(board)
+ship_col = random_col(board)
+print ship_row
+print ship_col
+
+for turn in range(4):
+    if turn == 3:
+        print "Game Over"
+    else:
+        guess_row = int(raw_input("Guess Row: "))
+        guess_col = int(raw_input("Guess Col: "))
+        
+        if guess_row == ship_row and guess_col == ship_col:
+            print "Enemy ship sunk"
+            break
+        else:
+            if (guess_row < 0 or guess_row > 4) or (guess_col <         0 or guess_col > 4):
+                print "Out of Bounds"
+            elif(board[guess_row][guess_col] == "X"):
+                print "Retry"
+            else:
+                print "Miss"
+                board[guess_row][guess_col] = "X"
+            print "Turn ", turn+1
+            print_board(board)
